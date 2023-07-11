7d76d6fa52e181f7f783f063b2a689c510f36f43
jiyoungjun <72366776+fatiger92@users.noreply.github.com>
2023-07-11 18:15:21 +0900

    c
    c

--- javaWorkspace/src/chap_08/_00_AccessModifierTest.java
+++ javaWorkspace/src/chap_08/_00_AccessModifierTest.java
@@ -1,6 +1,6 @@
 package chap_08;
 
-import chap_07.BlackBoxRefurbish;
+import chap_07.*; // *을 쓸 경우 해당 패키지의 모든 클래스를 사용하겠다는 것임.
 
 public class _00_AccessModifierTest {
     public static void main(String[] args) {
@@ -9,5 +9,7 @@ public class _00_AccessModifierTest {
         // b1.resolution = "FHD"; // default - 같은 패키지 내에서만 사용가능
         // b1.price = 200000; // private
         // b1.color = "블랙"; // protected
+
+        BlackBox b2;
     }
 }
